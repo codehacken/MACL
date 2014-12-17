0591ee6b5f3cdb8c1d6b3acd7d8aeef68ac72e7a
Codehacken <codehacken@gmail.com>
2014-12-16 20:46:09 -0500

    Update the gamerun.sh file for better start of the game

--- gamerun.sh
+++ gamerun.sh
@@ -1,8 +1,11 @@
-for ((  i = 0 ;  i <= $1;  i++  ))
+set -vx
+
+for ((  i = 0 ;  i < $1;  i++  ))
 do
-  java -jar tools/PlayGame.jar maps/map7.txt 5000 1000 log.txt "java -jar example_bots/RandomBot.jar" "python codes/MyRLBot.py $2 $3 $4" 2> "a${2}-${3}.out"
-  NOM=`grep "Turn" a${2}-${3}.out | wc -l`
-  WIN=`grep "Wins" a${2}-${3}.out`
+  java -jar tools/PlayGame.jar maps/map7.txt 5000 1000 log.txt "java -jar example_bots/RandomBot.jar" "python codes/RLMABot.py $2 $3 $4" 2> "results/a${2}-${3}.out"
+  NOM=`grep "Turn" results/a${2}-${3}.out | wc -l`
+  WIN=`grep "Wins" results/a${2}-${3}.out`
   echo "${NOM},${WIN}">> "results/Alpha${2}Gamma${3}.txt"
-  rm -f "a${2}-${3}.out"
+  #rm -f "a${2}-${3}.out"
 done
+
